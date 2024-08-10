3b4eab38a81d35a02fc77e91ec398323dfee4b38
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-10 11:59:40 +0530

    Mutable Example

---
+++ Mutable.py
@@ -0,0 +1,10 @@
+lst=[23,45,332,34]
+print(lst)
+lst.append(200)
+print(lst)
+#Now I will create same list
+
+lst1=[23,45,332,34,200]
+print(lst1)
+print(lst is lst1)
+# Here it will create 2 objects for the same list
