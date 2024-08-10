b3a63e25a2cd06835af4d8b265986d7040d89c82
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-10 12:00:09 +0530

    TypeCasting

---
+++ Typecasting.py
@@ -0,0 +1,26 @@
+a=99
+print(a)
+b=float(a)
+print(b)
+#---------------------float to complex-------------------
+
+a=76.5
+print(a)
+b=complex(a)
+print(b)
+
+# we can give the imaginary part
+
+x=34
+y=complex(x,3)
+print(y)
+
+#----- int to string------------
+
+a=34
+print(type(a))
+b=str(a)
+print(b)
+print(type(b))
+
+
