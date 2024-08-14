5cef55be2ed950f4cacfc52ef957d93ad5419a7a
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-14 12:23:35 +0530

    modules

---
+++ mymodule.py
@@ -0,0 +1,20 @@
+#file- A file from the hard disk when executed on command line then such a program or a file is called as a script.
+# If we have a single file with numerous set of functions then it is difficult to work with such file. The better way is to group similar set of functions and make a separate file of it and name accordingly.These files are called as modules.
+
+def add():
+    a=10
+    b=20
+    c=a+b
+    print(c)
+
+def sub():
+    a=10
+    b=20
+    c=b-a
+    print(c)
+
+def mul():
+    a=10
+    b=20
+    c=a*b
+    print(c)
