dacc3a8d9c18a979dc9cf9d64652d0073480db43
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-14 12:24:15 +0530

    example for module

---
+++ moduleEx.py
@@ -0,0 +1,27 @@
+import mymodule
+mymodule.add()
+mymodule.sub()
+mymodule.mul()
+
+#Another way
+
+import mymodule as mm
+mm.add()
+mm.sub()
+mm.mul()
+
+#Another Way
+
+from mymodule import*
+add()
+sub()
+mul()
+
+#Another Way
+
+from mymodule import  add,sub
+add()
+sub()
+mymodule.mul()
+
+
