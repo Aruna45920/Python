d383feb461c897bd47cc3d68422fc74b481dee84
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-14 12:27:10 +0530

    giving the input through user

---
+++ userinput.py
@@ -0,0 +1,21 @@
+print("Enter numerator")
+num=int(input())
+print("Enter denominator")
+den=int(input())
+
+res=num/den
+print(res)
+
+# Another way
+
+num=int(input("enter numerator\n"))
+den=int(input("enter denominator\n"))
+res=num/den
+print(res)
+
+
+#we have an build in one to evaluate the expressions it is called as eval
+
+exp=input("enter an expression\n")
+res=eval(exp)
+print(res)
