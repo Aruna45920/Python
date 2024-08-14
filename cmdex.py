dbcc4890ed0afa59ee7733ffc452ba10853fbf53
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-14 13:07:17 +0530

    commandline example

---
+++ cmdex.py
@@ -0,0 +1,9 @@
+# you can execute this in command prompt and you have to import sys
+
+import sys
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+
+res=int(sys.argv[1])/int(sys.argv[2])
+print(res)
