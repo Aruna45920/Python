a166c22c256b0b10cd125e4e4d3fba0225bc8f23
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-21 09:44:52 +0530

    Example for range

---
+++ range.py
@@ -0,0 +1,22 @@
+for i in[1,2,3,4,5]:
+    print("python")
+
+
+#In the above like that I have to print 100 time but it is not a good practice so now iam taking function called range
+    
+   
+for i in range(5):
+    print("Hi")
+
+
+#------------------------------------------------------------------------------
+
+   # print(list(range(5)))
+   
+
+print(list(range(2,10))) #here it prints from 2
+
+# now it prints 2,3,4,5 it steps one only
+# i want to print like 2,4,6,8 then i will write as range(2,10,2)
+
+print(list(range(2,11,2)))
